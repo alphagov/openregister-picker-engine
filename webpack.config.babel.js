01376d63b6c83b3082a9d924ff6dd441f121efd6
Theodor Vararu <theo@vararu.org>
2017-04-26 12:04:23 +0100

    Add webpack to compile bundle

---
+++ webpack.config.babel.js
@@ -0,0 +1,108 @@
+import webpack from 'webpack'
+import ReplacePlugin from 'replace-bundle-webpack-plugin'
+import path from 'path'
+import V8LazyParseWebpackPlugin from 'v8-lazy-parse-webpack-plugin'
+const ENV = process.env.NODE_ENV || 'development'
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'autocomplete-engine.min.js'
+  },
+
+  resolve: {
+    extensions: ['.js'],
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      'node_modules'
+    ]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: path.resolve(__dirname, 'src'),
+        enforce: 'pre',
+        loader: 'source-map-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+
+  plugins: ([
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(ENV)
+    })
+  ]).concat(ENV === 'production' ? [
+    new V8LazyParseWebpackPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      output: {
+        comments: false
+      },
+      compress: {
+        unsafe_comps: true,
+        properties: true,
+        keep_fargs: false,
+        pure_getters: true,
+        collapse_vars: true,
+        unsafe: true,
+        warnings: false,
+        screw_ie8: true,
+        sequences: true,
+        dead_code: true,
+        drop_debugger: true,
+        comparisons: true,
+        conditionals: true,
+        evaluate: true,
+        booleans: true,
+        loops: true,
+        unused: true,
+        hoist_funs: true,
+        if_return: true,
+        join_vars: true,
+        cascade: true,
+        drop_console: true
+      }
+    }),
+
+    // strip out babel-helper invariant checks
+    new ReplacePlugin([{
+      // this is actually the property name https://github.com/kimhou/replace-bundle-webpack-plugin/issues/1
+      partten: /throw\s+(new\s+)?[a-zA-Z]+Error\s*\(/g,
+      replacement: () => 'return;('
+    }])
+  ] : []),
+
+  stats: { colors: true },
+
+  node: {
+    global: true,
+    process: false,
+    Buffer: false,
+    __filename: false,
+    __dirname: false,
+    setImmediate: false
+  },
+
+  devtool: ENV === 'production' ? 'source-map' : 'cheap-module-eval-source-map',
+
+  devServer: {
+    port: process.env.PORT || 8080,
+    host: 'localhost',
+    publicPath: '/dist/',
+    contentBase: './examples',
+    watchContentBase: true,
+    historyApiFallback: true,
+    open: true
+  }
+}
